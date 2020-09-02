2959ea8745435936b3a4a74292675412a94df6fc
Kitkathawk <susana.vale.lopes@gmail.com>
2020-09-03 00:24:43 +0100

    Added jQuery

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+//DROP-DOWN MENU FOR MOBILE HEADER
+
+$(document).ready(function(){
+
+  $('.fa-ellipsis-v').on('click', function(event){
+
+  	$('.mobile-menu').toggle(500);
+  	$('.profile-pic').toggleClass('active');
+
+	  	if ( $('.profile-pic').hasClass('active') ) {
+
+	  		$('.profile-pic').css('margin-top', '50px');
+
+	  	} else {
+
+	  		$('.profile-pic').css('margin-top', '0px');
+
+	  	}
+
+  }); 
+  
+ 
+//START BARS ANIMATION ONLY WHEN ELEMENT VISIBLE
+
+	$(document).on('scroll', function(event){
+
+		let scrollOffset = $(document).scrollTop();
+		let elementOffset = $('#anchor').offset().top - scrollOffset;
+
+		if (scrollOffset > elementOffset) {
+
+			$('.bar-graph--html').animate({width: '90%'}, 700, 'swing');
+			$('.bar-graph--css').animate({width: '80%'}, 800, 'swing');
+			$('.bar-graph--js').animate({width: '50%'}, 900, 'swing');
+			$('.bar-graph--sql').animate({width: '70%'}, 1000, 'swing');
+			$('.bar-graph--php').animate({width: '40%'}, 1100, 'swing');
+
+		}
+	});
+
+});
